c0b6bf6fbf9928ad6afc6d392a253b37b7695610
Parth Kalia <95091403+TheParthK@users.noreply.github.com>
2022-10-02 14:11:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 from streamlit_webrtc import webrtc_streamer, RTCConfiguration
 import av
 import cv2
+import streamlit as st
+import tensorflow as tf
+import numpy as np
 
+trained_model = tf.keras.models.load_model('facial_emotions_model.h5')
+classes=['Angry','Disgust','Fear','Happy','Neutral','Sad','Surprise']
 cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 class VideoProcessor:
 	def recv(self, frame):
 		frm = frame.to_ndarray(format="bgr24")
+		gray = cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY)
+
 
 		faces = cascade.detectMultiScale(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY), 1.1, 3)
 
 		for x,y,w,h in faces:
 			cv2.rectangle(frm, (x,y), (x+w, y+h), (0,255,0), 3)
+			frm = frm[y:y+h, x:x+h]
 
 		return av.VideoFrame.from_ndarray(frm, format='bgr24')
 
